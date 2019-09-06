acfcfdb17c10d5e70e5aa9fd564a9509c9547973
Pravien <Pravien95@gmail.com>
2019-09-06 22:42:51 +0200

    finished the part about average prices

--- assignment-2/assignment-2.py
+++ assignment-2/assignment-2.py
@@ -4,6 +4,7 @@ import pandas as pd
 from tqdm import tqdm
 from geopy.geocoders import Nominatim
 from statistics import mean
+import requests
 
 
 def get_locations(address, zip_code):
@@ -17,9 +18,7 @@ def get_locations(address, zip_code):
         search_address = ', '.join([address_field, zip_field])
 
         geolocator = Nominatim()
-        print('here')
         location = geolocator.geocode(search_address)
-        print('after')
         return location.latitude, location.longitude
     except:
         print('Skipped geocoding of {} {}'.format(address, zip_code))
@@ -57,13 +56,19 @@ def create_city_csv(dataframe, year):
 
 
 if __name__ == '__main__':
-    df = pd.read_csv('./test.csv', index_col=None, header=0)
-    #enable to get geocodes. #df['lat'],df['long']= zip(*df.apply (lambda row: get_locations(row['address'].split(',')[0],row['zip_code']), axis=1))
+
+    # read the csv.
+    df = create_data_frame()
+    # Add 2 new columns for latitude and longitude and writes i to a csv file.
+    df['lat'],df['long']= zip(*df.apply (lambda row: get_locations(row['address'].split(',')[0],row['zip_code']), axis=1))
+    df.to_csv("lat-long.csv", index=False,encoding='utf-8')
+
+
+    
+    '''
     #  convert the column to a datetime object.
     df['sell_date'] = pd.to_datetime(df['sell_date'],format='%d-%m-%Y')
     df['zip_code_num'] = df.apply(lambda x: remove_city_name(x['zip_code']), axis=1)
-   #print(df['sell_date'][0].year)
-    #print(df['zip_code_num'])
     mask_1992 = ((df['sell_date'].dt.year == 1992) & 
      ((df['zip_code_num'] == '1050') | (df['zip_code_num'] == '1049') | (df['zip_code_num'] == '5000') | 
      (df['zip_code_num'] == '8000') | (df['zip_code_num'] == '9000'))
@@ -74,6 +79,8 @@ if __name__ == '__main__':
      (['zip_code_num'] == '8000') | (['zip_code_num'] == '9000'))
      )
 
-    create_city_csv(df[mask_1992], '2016')
+    create_city_csv(df[mask_1992], '1992')
     print('Average price per square meter for the year 1992 is {} m\u00b2'.format(mean(df[mask_1992]['price_per_sq_m'])),)
     print('Average price per square meter for the year 2016 is {} m\u00b2'.format(mean(df[mask_2016]['price_per_sq_m'])),)
+
+    '''
